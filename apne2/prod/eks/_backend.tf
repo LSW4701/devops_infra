e59f7b65b15b2a8e84db2ab7c1b09e98bf36d0ab
lsw4701 <pro_4701@naver.com>
2022-10-14 00:27:21 +0900

    ㄹㅇ

--- apne2/prod/eks/_backend.tf
+++ apne2/prod/eks/_backend.tf
@@ -1,11 +1,19 @@
 terraform {
+
+  
   backend "s3" {
-    bucket = "devops-prod-tfbackend-s3"
-    key    = "apne2/eks/mgt/terraform.tfstate"
+    bucket = "lsw-bucket"             #   버켓명 
+    key = "terra2/terraform.tfstate"  #  생성되는 폴더/파일명  
     region = "ap-northeast-2"
-    #    role_arn    = "{ASSUMED_ROLE}"
-    max_retries = 3
   }
+
+  # backend "s3" {
+  #   bucket = "devops-prod-tfbackend-s3"
+  #   key    = "apne2/eks/mgt/terraform.tfstate"
+  #   region = "ap-northeast-2"
+  #   #    role_arn    = "{ASSUMED_ROLE}"
+  #   max_retries = 3
+  # }
 
   required_providers {
     kubernetes = {
