8244236c1f0ebd00d72e4c7f002cde7b2db927c5
lsw4701 <pro_4701@naver.com>
2022-10-14 17:23:35 +0900

    sds

--- apne2/prod/network/vpc/_backend.tf
+++ apne2/prod/network/vpc/_backend.tf
@@ -1,11 +1,17 @@
 terraform {
 
-
-   backend "s3" {
-    bucket = "lsw-bucket"             #   버켓명 
-    key = "terra4/terraform.tfstate"  #  생성되는 폴더/파일명  
-    region = "ap-northeast-2"
+  backend "local" {
   }
+
+
+  #  backend "s3" {
+  #   bucket = "lsw-bucket"             #   버켓명 
+  #   key = "terra4/terraform.tfstate"  #  생성되는 폴더/파일명  
+  #   region = "ap-northeast-2"
+  # }
+
+
+
 #   backend "s3" {
 #     bucket      = "devops-prod-tfbackend-s3"
 #     key         = "apne2/network/vpc/terraform.tfstate"
