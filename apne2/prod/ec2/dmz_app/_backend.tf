bdefb8cb24ea7af90669241f7462ee4887992763
lsw4701 <pro_4701@naver.com>
2022-10-14 18:36:18 +0900

    df

--- apne2/prod/ec2/dmz_app/_backend.tf
+++ apne2/prod/ec2/dmz_app/_backend.tf
@@ -4,7 +4,7 @@ terraform {
     bucket = "lsw-bbk"             #   버켓명 
     key = "terra1/terraform.tfstate"  #  생성되는 폴더/파일명  
     region = "ap-northeast-2"
-    role_arn    = "arn:aws:iam::552661052297:role/terraform-runner-role"
+    
     max_retries = 3
   }
 
