3584923c3fd0c3a5135aae08762d2ebd9e1d427d
lsw4701 <pro_4701@naver.com>
2022-10-17 03:36:08 +0900

    df

--- apne2/prod/ec2/dmz_app/_provider.tf
+++ apne2/prod/ec2/dmz_app/_provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = local.region
-  aws_access_key_id = AKIA5643M7CC35SXYOCQ
-  aws_secret_access_key = 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
+  access_key = "AKIA5643M7CC35SXYOCQ"
+  secret_key = "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"
 #  assume_role {
 #    role_arn     = "{ASSUMED_ROLE}"
 #    session_name = "{SESSION_NAME}"
