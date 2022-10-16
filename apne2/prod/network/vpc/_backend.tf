fa196328aa4df43d753e235810852d09df2b34d7
lsw4701 <pro_4701@naver.com>
2022-10-17 08:32:03 +0900

    df

--- apne2/prod/network/vpc/_backend.tf
+++ apne2/prod/network/vpc/_backend.tf
@@ -6,6 +6,7 @@ terraform {
    backend "s3" {
     bucket = "lsw-bbk1"             #   버켓명 
     key = "apne2/network/vpc/terraform.tfstate"  #  생성되는 폴더/파일명  
+    role_arn    = "arn:aws:iam::893923049642:role/rolel"
     region = "ap-northeast-2"
   }
 
