3e3e3de6d0ff51f8b33c561ccef0b5ae05759cdb
lsw4701 <pro_4701@naver.com>
2022-10-23 04:59:00 +0900

    fd

--- create.sh
+++ create.sh
@@ -1,6 +1,6 @@
 DIR="$( cd "$( dirname "$0" )" && pwd -P )"
 
-cd $DIR/apne2/prod/network ; terraform init
+cd $DIR/apne2/prod/network/vpc; terraform init
 terraform apply -auto-approve
 
 cd $DIR/apne2/prod/ec2/dmz_app ; terraform init
--- destroy.sh
+++ destroy.sh
@@ -3,13 +3,13 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd -P )"
 
-cd $DIR/apne2/prod/ec2/dmz_app ; terraform init
+cd $DIR/apne2/prod/ec2/dmz_app ; 
 terraform destroy -auto-approve
 
-cd $DIR/apne2/prod/ec2/bastion ; terraform init
+cd $DIR/apne2/prod/ec2/bastion ; 
 terraform destroy -auto-approve
 
-cd $DIR/apne2/prod/network/vpc ; terraform init
+cd $DIR/apne2/prod/network/vpc ; 
 terraform destroy -auto-approve
 
 
