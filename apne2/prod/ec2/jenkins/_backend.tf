48f796a1f027ed28f757baf63b67ca163794a818
lsw4701 <pro_4701@naver.com>
2022-10-24 02:32:50 +0900

    sfs

---
+++ apne2/prod/ec2/jenkins/_backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket      = "devops-prod-tfbackend-s3"
+    key         = "apne2/apne2/ec2/jenkins/terraform.tfstate"
+    region      = "ap-northeast-2"
+    role_arn    = "arn:aws:iam::552661052297:role/terraform-runner-role"
+    max_retries = 3
+  }
+}
+
+
+# terraform {
+#   backend "s3" {
+#     bucket      = "devops-dev-tfbackend-s3"
+#     key         = "dev/apne2/ec2/jenkins/terraform.tfstate"
+#     region      = "ap-northeast-2"
+# #    role_arn    = "{ASSUMED_ROLE}"
+#     max_retries = 3
+#   }
+# }
--- create.sh
+++ create.sh
@@ -1,10 +1,7 @@
 DIR="$( cd "$( dirname "$0" )" && pwd -P )"
 
-cd $DIR/apne2/prod/network/vpc; terraform init
-terraform apply -auto-approve
+cd $DIR/apne2/prod/network/vpc ; terraform init ; terraform apply -auto-approve
 
-cd $DIR/apne2/prod/ec2/dmz_app; terraform init
-terraform apply -auto-approve
+cd $DIR/apne2/prod/ec2/dmz_app ; terraform init ; terraform apply -auto-approve
 
-cd $DIR/apne2/prod/ec2/bastion ; terraform init
-terraform apply -auto-approve
+cd $DIR/apne2/prod/ec2/bastion ; terraform init ; terraform apply -auto-approve
