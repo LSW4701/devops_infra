aa4d54c176cbd64f397c5539ea6c43c9afcf7d43
lsw4701 <pro_4701@naver.com>
2022-10-24 02:38:47 +0900

    df

--- apne2/prod/ec2/jenkins/_backend.tf
+++ apne2/prod/ec2/jenkins/_backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket      = "devops-prod-tfbackend-s3"
     key         = "apne2/apne2/ec2/jenkins/terraform.tfstate"
     region      = "ap-northeast-2"
-    # role_arn    = "arn:aws:iam::552661052297:role/terraform-runner-role"
+    role_arn    = "arn:aws:iam::959714228357:role/terraformAsume"
     max_retries = 3
   }
 }
