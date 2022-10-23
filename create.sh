068bbb245dda7e23e917dee2aaa349af8d2c5060
lsw4701 <pro_4701@naver.com>
2022-10-24 04:40:52 +0900

    ㅇㄹ

--- create.sh
+++ create.sh
@@ -5,3 +5,8 @@ cd $DIR/apne2/prod/network/vpc ; terraform init ; terraform apply -auto-approve
 cd $DIR/apne2/prod/ec2/dmz_app ; terraform init ; terraform apply -auto-approve
 
 cd $DIR/apne2/prod/ec2/bastion ; terraform init ; terraform apply -auto-approve
+
+cd $DIR/apne2/prod/ec2/jenkins; terraform init ; terraform apply -auto-approve
+
+cd $DIR/apne2/prod/alb/jenkins ; terraform init ; terraform apply -auto-approve
+
--- destroy.sh
+++ destroy.sh
@@ -3,15 +3,21 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd -P )"
 
+cd $DIR/apne2/prod/alb/jenkins ; 
+terraform destroy -auto-approve
+
+cd $DIR/apne2/prod/ec2/jenkins ; 
+terraform destroy -auto-approve
+
 cd $DIR/apne2/prod/ec2/dmz_app ; 
 terraform destroy -auto-approve
 
 cd $DIR/apne2/prod/ec2/bastion ; 
 terraform destroy -auto-approve
 
-cd $DIR/apne2/prod/network/vpc ; 
-terraform destroy -auto-approve
-
+# cd $DIR/apne2/prod/network/vpc ; 
+# terraform destroy -auto-approve
+# vpc부분 잠정대기 
 
 
 
