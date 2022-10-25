36421a666deb616d8a420de25eeeff937a3519ab
lsw4701 <pro_4701@naver.com>
2022-10-25 18:16:51 +0900

    df

--- apne2/prod/lambda2/cw-log-subscriber/_backend.tf
+++ apne2/prod/lambda2/cw-log-subscriber/_backend.tf
@@ -12,7 +12,7 @@ terraform {
 
    backend "s3" {
     bucket = "lsw-bk"             #   버켓명 
-    key = "/apne2/lambda/cw-log-subscriber/terraform.tfstate" #  생성되는 폴더/파일명  
+    key = "apne2/lambda/cw-log-subscriber/terraform.tfstate" #  생성되는 폴더/파일명  
     role_arn    = "arn:aws:iam::959714228357:role/terraformAsume"
     region = "ap-northeast-2"
   }
